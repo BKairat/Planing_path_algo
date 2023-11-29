improove RRT, need to fix rotation

configurations.py
@@ -3,13 +3,18 @@ import numpy as np
 
 class Configuration():
     def __init__(self,
-                 config: (tuple, float),
+                 trans: np.array,
+                 rot: float | np.array,
                  parent: Configuration | None = None
                  ):
-        self.config: (tuple, float) = config
+        self.trans = trans
+        self.rot = rot
         self.parent: Configuration = parent
 
-    def get_act(self, other: Configuration) -> (np.array, float):
-        translation = np.array(other.config[0])-np.array(self.config[0])
-        rotation = other.config[1] - self.config[0]
+    def get_act(self, other: Configuration) -> (np.array, float | np.array):
+        translation = other.trans - self.trans
+        rotation = other.rot - self.rot
         return (translation, rotation)
+
+    def copy(self) -> Configuration:
+        return Configuration(np.copy(self.trans), np.copy(self.rot), self.parent)

datasets.py
@@ -1,12 +1,17 @@
 from __future__ import annotations
+
+import copy
 import os
 import shutil
 import torch
 import pandas as pd
 from gui import Render
 from object import Object3D
+from configurations import Configuration
+import pygame as pg
 import numpy as np
-import rapidmodule
+from planing_algorithms import RRT
+from random import uniform
 
 
 def copy_file_to_folder(file: str, dest_folder: str) -> str:
@@ -60,27 +65,80 @@ class Dataset():
 
     def show(self):
         app = Render()
-        robot = Object3D(None, path = self.rob, scale=0.2)
+        robot = Object3D(None, path = self.rob, scale=0.2, l_color=pg.Color('blue'))
         goal = Object3D(None, path = self.goal)
+        robot.set_to(Configuration(self.config['robot.obj'][0],
+                                   self.config['robot.obj'][1],
+                                   None),
+                     change_c=True)
+        goal.set_to(Configuration(self.config['goal.obj'][0], self.config['goal.obj'][1], None), change_c=True)
 
-        robot.movement(*self.config['robot.obj'])
-        goal.movement(*self.config['goal.obj'])
+        # a = np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])
+
+        # seq = [
+        #     Configuration(np.array([10, 0, 0]),
+        #                   a),
+        #     Configuration(np.array([10, 0, 0]),
+        #                   -a),
+        #     Configuration(np.array([10, 0, 0]),
+        #                   a),
+        #     Configuration(np.array([10, 0, 0]),
+        #                   -a),
+        #     Configuration(np.array([10, 0, 0]),
+        #                   a),
+        #     Configuration(np.array([10, 0, 0]),
+        #                   -a),
+            # Configuration(np.array([20, 0, 20]), np.array([0, 0, 0]))
+        # ]
+
+        # seq = [
+        #     Configuration(np.array([10, 0, 0]), np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])),
+        #     Configuration(np.array([10, 0, 10]), np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])),
+        #     Configuration(np.array([10, 0, 10]), np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])),
+        #     Configuration(np.array([10, 3450, 10]), np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])),
+        #     Configuration(np.array([1780, 0, 10]), np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])),
+        #     Configuration(np.array([10, 0, 10]), np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])),
+        #     Configuration(np.array([10, 0, 178560]), np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])),
+        #     Configuration(np.array([10, 78, 10]), np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])),
+        #     Configuration(np.array([1078, 0, 1045]), np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])),
+        #     Configuration(np.array([20, 0, 20]), np.array([0, 0, 0]))
+        #     ]
+        # print("robot config", robot.config.trans, robot.config.rot)
+        # for conf in seq:
+        #     robot.set_to(conf)
+        # print("robot config", robot.in_collision(goal))
 
         app.add_object(robot)
         app.add_object(goal)
 
+        obs = []
+
         for o in self.obst:
             obst = Object3D(None, path=o)
-            obst.movement(*self.config[os.path.basename(o)])
+            obst.set_to(Configuration(self.config[os.path.basename(o)][0], self.config[os.path.basename(o)][1], None), change_c=True)
+            obs.append(obst)
             app.add_object(obst)
-        p = np.array([self.data.iloc[i][1:4] for i in range(len(self.data))])
-        p = np.insert(p, 3, np.ones(len(self.data)), axis = 1)
+        algo = RRT(robot=robot.copy_o(), goal=goal, map_size=(25, 0, 25), obstacles=obs)
+
+        algo.plan()
+        # p = np.array([self.data.iloc[i][1:4] for i in range(len(self.data))])
+        # p = np.insert(p, 3, np.ones(len(self.data)), axis = 1)
+        # p = np.array([i.trans.tolist() + [1] for i in algo.G])
+
+        diff_path1 = Object3D(None,
+                             points=[g.trans.tolist() + [1] for g in algo.G],
+                             faces=[i for i in range(len(algo.G))])
+        diff_path1.draw_points = True
+        app.add_object(diff_path1)
+
         diff_path = Object3D(None,
-                             points=p,
-                             faces=[i for i in range(len(self.data))])
+                             points=[robot.config.trans.tolist() + [1] for _ in range(3)],
+                             faces=[i for i in range(3)])
         diff_path.draw_points = True
         app.add_object(diff_path)
-
+        # print("datasets/robot_conf", goal.config.trans, goal.config.rot)
+        # for i in algo.G:
+            # print(i.trans)
         app.run()
 
 

object.py
@@ -6,6 +6,7 @@ from transformation_matrix import *
 from camera import *
 from projection import *
 from numba import njit
+from rapidmodule import collision
 
 @njit(fastmath=True)
 def any_(arr: ArrayLike, a: float, b: float) -> bool:
@@ -49,16 +50,21 @@ class Object3D():
                  faces: ArrayLike = np.array([(0, 1, 2, 3), (4, 5, 6, 7), (0, 4, 5, 1),
                                         (2, 3, 7, 6), (1, 2, 6, 5), (0, 3, 7, 4)]),
                  path: str | None = None,
-                 scale: float | None = None
+                 scale: float | None = None,
+                 p_color: tuple | pg.Color = pg.Color('red'),
+                 l_color: tuple | pg.Color = pg.Color('orange'),
                  ):
+        self.config = None
         self.render = render
         if path is None:
             self.points = points
             self.faces = faces
         else:
             self.points, self.faces = self.get_from_obj_file(path, scale)
+
         self.font = pg.font.SysFont('Arial', 30, bold=True)
-        self.color_faces = [(pg.Color('orange'), face) for face in self.faces]
+        self.color_faces = [(l_color, face) for face in self.faces]
+        self.p_color = p_color
         self.movement_flag = True
         self.label = ''
         self.draw_points = False
@@ -79,9 +85,9 @@ class Object3D():
         x = (max(points, key = lambda x: x[0])[0] + min(points, key = lambda x: x[0])[0])/2
         y = (max(points, key = lambda x: x[1])[1] + min(points, key = lambda x: x[1])[1])/2
         z = min(points, key = lambda x: x[2])[2]
-        self.ref_point = np.array([x, y, z, 0])
-        points = points - self.ref_point
-        self.ref_point -= self.ref_point
+        ref_point = np.array([x, y, z, 0])
+        points = points - ref_point
+        self.config = Configuration(ref_point[:-1], np.zeros(3))
         return ([np.array(p) for p in points],
                 [np.array(f) for f in faces])
 
@@ -89,7 +95,11 @@ class Object3D():
         self.screen_projection()
 
     def translate(self, trans: ArrayLike) -> None:
+        self.config.trans += trans
         self.points = self.points @ translate(trans)
+
+    def in_collision(self, other: Object3D) -> bool:
+        return collision(self.rapid_mod(), other.rapid_mod())
 
     def screen_projection(self):
         points = self.points @ self.render.camera.camera_matrix()
@@ -103,37 +113,62 @@ class Object3D():
             for index, point in enumerate(points):
                 if not any_(point, self.render.w_width, self.render.w_height):
                     pg.draw.circle(
-                        self.render.screen, pg.Color('red'),
+                        self.render.screen, self.p_color,
                         point, 10 * np.exp(-0.005*np.sqrt(sum((self.points[index] - self.render.camera.position) ** 2)))
                     )
         else:
             for index, color_face in enumerate(self.color_faces):
                 color, face = color_face
                 polygon = points[face]
-                # print("\n\n\n",polygon,"\n\n\n")
                 if not any_(polygon, self.render.w_width, self.render.w_height):
-                    # if not np.any(polygon == self.render.w_width) or np.any(polygon == self.render.w_height):
                     pg.draw.polygon(self.render.screen, color, polygon, 1)
                     if self.label:
                         text = self.font.render(self.label[index], True, pg.Color('black'))
                         self.render.screen.blit(text, polygon[-1])
 
-    def movement(self, trans: np.array, angles: np.array):
+    def set_to(self, configuration: Configuration, change_c: bool = False):
+        trans, rot = self.config.get_act(configuration)
+        # print(f">>> {rot}")
         self.translate(trans)
-        self.rotation(angles)
-        # if self.movement_flag:
-            # self.translate(np.array([0.005,0,0]))
-            # self.rotation([pg.time.get_ticks() % 0.005, pg.time.get_ticks() % 0.005, pg.time.get_ticks() % 0.005])
+        self.rotation(-self.config.rot)
+        self.rotation(configuration.rot)
+        if change_c:
+            min_y = min(self.points, key=lambda x: x[2])[1]
+            p = []
+            for i in self.points:
+                if np.allclose(i[1], min_y):
+                    p.append(i)
+            trans = min(p, key=lambda x: sum(x))
+            self.config.trans = np.array(trans[:-1])
+            self.config.rot = np.zeros(3)
+
+    def act(self, trans: np.array, rot: np.array) -> None:
+        self.translate(trans)
+        self.rotation(rot)
+
 
     def rotation(self, angles: ArrayLike) -> None:
         assert len(angles) == 3, 'rotation in 3D space must receive 3 angles (len(angles) == 3)'
+        pos = np.array(self.config.trans)
+        # print("-->",pos)
+        self.translate(-pos)
         self.points = self.points @ rotate_x(angles[0])
         self.points = self.points @ rotate_y(angles[1])
         self.points = self.points @ rotate_z(angles[2])
+        self.config.rot += angles
+        self.translate(pos)
 
     def rapid_mod(self) -> list:
-        points = self.points.tolist()
         return [self.points[face].tolist() for face in self.faces]
+
+    def dist_to(self, other: Object3D) -> float:
+        return np.sqrt(sum((self.config.trans - other.config.trans)**2))
+
+    def copy_o(self) -> Object3D:
+        ret = Object3D(None,points=np.copy(self.points),
+                       faces=np.copy(self.faces))
+        ret.config = self.config.copy()
+        return ret
 
 
 
@@ -148,3 +183,4 @@ class Axes(Object3D):
         self.colors = [pg.Color(col) for col in ['red', 'green', 'blue']]
         self.color_faces = [(color, face) for color, face in zip(self.colors, self.faces)]
         self.label = 'XYZ'
+        self.config = Configuration(trans=np.zeros(3), rot=np.zeros(3))

planing_algorithms.py
@@ -1,53 +1,125 @@
 from __future__ import annotations
-from object import Object2D
+from object import Object2D, Object3D
 from configurations import Configuration
 from random import uniform, choice
 import numpy as np
+import copy
 
 class RRT():
     def __init__(self,
-                 robot: Object2D,
+                 robot: Object2D | Object3D,
+                 goal: Object2D | Object3D,
                  map_size: tuple,
-                 goal: tuple,
-                 delta_t: float = 50,
-                 threshold: float = 50,
-                 obstacles: [Object2D] = []
+                 delta_t: float = 5,
+                 threshold: float = 5,
+                 obstacles: [Object2D | Object3D] = [],
+                 iterations: int = 5
                  ):
         assert len(map_size) >= 2, ("RRT algorithm requires 2 or more dimensional space,"
                                     " map_size must contain at least 2 elements")
         self.dims = len(map_size)
-        self.G: [Configuration] = [robot.config]
-        self.robot: Object2D = robot
-        self.obstacles: [Object2D] = obstacles
+        self.G: [Configuration] = [Configuration(robot.config.trans, robot.config.rot, None)]
+        # print("planing path init:", self.G[0].trans)
+        self.robot: Object2D | Object3D = robot
+        self.goal: Object2D | Object3D = goal
+        self.obstacles: [Object2D | Object3D] = obstacles
         self.map_size: tuple = map_size
         self.delta_t: float = delta_t
-        self.goal: tuple = goal
         self.threshold: float = threshold
+        self.iterations = iterations
 
     def random_act(self) -> Configuration:
-        angle = uniform(-np.pi, np.pi)
-        random_point = (uniform(0, i) for i in self.map_size)
+        random_angle = np.array([uniform(-np.pi, np.pi), uniform(-np.pi, np.pi), uniform(-np.pi, np.pi)])
+        random_point = np.array([uniform(0, i) for i in self.map_size])
+        # print(f">>>randon func: {random_point}, {random_angle}")
         # TODO: add finding the nearest neighborhood #
-        nearest_conf = choice(self.G)
+        nearest_conf = self.nearest_n(random_point)
         # ------------------------------------------ #
-        translation = (self.delta_t * (np.array(random_point) - nearest_conf.config[0])
-                       / np.linalg.norm(np.array(random_point) - nearest_conf.config[0]))
+        translation = (self.delta_t * (random_point - nearest_conf.trans)
+                       / np.linalg.norm(random_point - nearest_conf.trans))
+        rotation = random_angle - nearest_conf.rot
         new_configuration = Configuration(
-            config=(tuple(translation), angle),
+            trans=translation,
+            rot=rotation,
             parent=nearest_conf
         )
+        # print(f"\nfunc: act_to_rand: trans {new_configuration.trans}, rot: {new_configuration.rot}")
         return new_configuration
 
-    def check_task(self, position: tuple | np.array) -> bool:
-        return np.linalg.norm(np.array(position) - self.goal) < self.threshold
+    def act_to_goal(self) -> Configuration:
+        nearest_conf = self.nearest_n(self.goal.config.trans)
+        # print("act_to_goal/nearest conf translation", nearest_conf.trans)
+        # print("act_to_goal/delta_t", self.delta_t)
+        # print("sct_to_goal/goal", self.goal.config.trans)
+        translation = (self.delta_t * (np.array(self.goal.config.trans) - nearest_conf.trans)
+                       / np.linalg.norm(np.array(self.goal.config.trans) - nearest_conf.trans))
+        # print("act_to_goal/translation", translation)
+        rotation = np.zeros(len(self.map_size))
+        new_configuration = Configuration(
+            trans = translation,
+            rot = rotation,
+            parent=nearest_conf
+        )
+        # print(f"\nfunc: act_to_goal: trans {new_configuration.trans}, rot: {new_configuration.rot}")
+        return new_configuration
+
+
+    def check_task(self, conf: Configuration) -> bool:
+        self.robot.set_to(conf)
+        return self.robot.in_collision(self.goal)
 
     def add_conf(self, configuration: Configuration):
         self.G.append(configuration)
 
-    def is_feasible_action(self, config: Configuration) -> [Configuration] | None:
+    def nearest_n(self, point: np.array) -> Configuration:
+        # print(">>>nearest", min(self.G, key = lambda x: sum((x.trans - point)**2)).trans, min(self.G, key = lambda x: sum((x.trans - point)**2)).rot)
+        return min(self.G, key = lambda x: sum((x.trans - point)**2))
+
+    def is_feasible_action(self, desired: Configuration) -> bool:
         # TODO: add collision detection
-        points = self.robot.points
+        # return True
+        if desired.parent == None:
+            return True
+        transl, rot = desired.parent.get_act(desired)
+        i_t = transl / self.iterations
+        i_r = rot / self.iterations
+        self.robot.set_to(desired.parent)
+        for i in range(self.iterations):
+            self.robot.act(i_t, i_r)
+            for obstacle in self.obstacles:
+                if self.robot.in_collision(obstacle):
+                    return False
+        return True
 
-        end_c = config
+    def plan(self) -> [Configuration]:
+        init_conf = copy.deepcopy(self.robot.config)
+        max_iteration = 200
+        new_conf = None
+        path = []
+        # print("\n\n\n============================= start algo =============================")
+        for iter in range(max_iteration):
+            print("iter", iter)
+            while True:
+                if iter % 20 == 0:
+                    new_conf = self.act_to_goal()
+                else:
+                    new_conf = self.random_act()
 
-        return None
+                # print(f"--> New config: trans: {new_conf.trans}, rot: {new_conf.rot}")
+
+                if self.is_feasible_action(new_conf):
+                    if iter % 20 == 0:
+                        iter += 1
+                    break
+            self.add_conf(new_conf)
+
+            if self.check_task(new_conf):
+                cur = new_conf
+                while cur.parent:
+                    path.append[cur]
+                    cur = cur.parent
+                return path
+        # print(init_conf.trans, init_conf.rot)
+        self.robot.set_to(init_conf)
+
+

test.py
@@ -1,4 +1,4 @@
-import rapidmodule
+from rapidmodule import collision
 import numpy as np
 from transformation_matrix import *
 a = [[[0, 0, 0], [10, 0, 0], [0, 10, 0]],
@@ -17,5 +17,5 @@ b = np.delete(b, -1, -1)
 b = b.tolist()
 # print(b)
 
-print(rapidmodule.collision(a,b))
+print(collision(a,b))
 #